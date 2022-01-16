0df1c1f537d7544e562a8991af66c33c69eef965
drakotech <george.flefel@gmail.com>
2022-01-15 23:39:42 -0800

    Setup logo component and styles

---
+++ src/components/logo.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+export default class Logo extends Component {
+  render() {
+    return (
+      <div className="logo-main">
+        <img
+          alt="Daily Smarty UI image logo big"
+          src="/assets/ds_circle_logo.png"
+        />
+      </div>
+    );
+  }
+}
