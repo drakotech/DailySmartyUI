adfb719708ffdfa9a0941fd5eb6852bc68b4dca1
drakotech <george.flefel@gmail.com>
2022-01-20 16:39:41 -0800

    Used axios to perform get request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,20 @@
+import { SET_RECENT_POSTS } from "./types";
+
+import axios from "axios";
+
 export function fetchRecentPosts() {
   return function (dispatch) {
-    //perform our request in here.
-    console.log("Hello");
+    axios
+      .get("https://api.dailysmarty.com/posts")
+      .then((response) => {
+        console.log(response.data.posts);
+        dispatch({
+          type: SET_RECENT_POSTS,
+          payload: response.data.posts,
+        });
+      })
+      .catch((error) => {
+        console.log("error from fetchRecentPosts", error);
+      });
   };
 }
