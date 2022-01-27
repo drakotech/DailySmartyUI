81bee72167ac7726638b958771be0f48f90cf60f
drakotech <george.flefel@gmail.com>
2022-01-26 19:00:01 -0800

    Put together fetchPostsWithQuery action to receive queried posts.

--- src/components/home.js
+++ src/components/home.js
@@ -3,11 +3,14 @@ import Logo from "./logo";
 import SearchBar from "./searchBar";
 import RecentPosts from "./recentPosts";
 
-export default class Home extends Component {
-  handleSearchBarSubmit = function ({ query }) {
-    console.log("trying to handle submit for query", query);
+import { connect } from "react-redux";
+import * as actions from "../actions";
+
+class Home extends Component {
+  handleSearchBarSubmit(query) {
+    this.props.fetchPostsWithQuery(query);
     this.props.history.push("/results");
-  };
+  }
 
   render() {
     return (
@@ -21,3 +24,5 @@ export default class Home extends Component {
     );
   }
 }
+
+export default connect(null, actions)(Home);
--- src/components/results.js
+++ src/components/results.js
@@ -3,9 +3,12 @@ import React, { Component } from "react";
 import Logo from "./logo";
 import SearchBar from "./searchBar";
 
-export default class Results extends Component {
+import { connect } from "react-redux";
+import * as actions from "../actions";
+
+class Results extends Component {
   handleSearchBarSubmit(query) {
-    console.log("query: ", query);
+    this.props.fetchPostsWithQuery(query);
   }
 
   render() {
@@ -17,3 +20,5 @@ export default class Results extends Component {
     );
   }
 }
+
+export default connect(null, actions)(Results);
